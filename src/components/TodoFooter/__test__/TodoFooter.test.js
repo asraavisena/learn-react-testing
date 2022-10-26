f8e91d83f80184860b94f824cd4655a03e595082
asraavisena <asra.avisena@yahoo.com>
2022-10-26 14:34:46 +0700

    adding describe block

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -12,26 +12,29 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
   )
 }
 
-it("Should render incompleted tasks", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+describe("Todo footer", () => {
+  it("Should render incompleted tasks", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 
-it("Should render incompleted task number incomplete task = 1", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+  it("Should render incompleted task number incomplete task = 1", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 
-it("Should render incompleted task number incomplete task = 1 TEST ASSERTION", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByTestId("id-para");
-  expect(paragraphElement).toHaveTextContent("1 task left");
-});
+  it("Should render incompleted task number incomplete task = 1 TEST ASSERTION", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByTestId("id-para");
+    expect(paragraphElement).toHaveTextContent("1 task left");
+  });
 
-it("Should render incompleted task number incomplete task = 1 TEST ASSERTION another 1", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByTestId("id-para");
-  expect(paragraphElement.textContent).toBe("1 task left");
-});
+  it("Should render incompleted task number incomplete task = 1 TEST ASSERTION another 1", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByTestId("id-para");
+    expect(paragraphElement.textContent).toBe("1 task left");
+  });
+
+})
